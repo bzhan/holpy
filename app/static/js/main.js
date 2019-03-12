a5eb65206c9ad202d3db63165a24edc1e010aaa9
Bella96 <731977292@qq.com>
2019-03-12 15:44:09 +0800

    ''

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -293,9 +293,6 @@
             var a_id = $(this).attr('id').trim();
             var number = Number(a_id.slice(5,)) - 1;
             $('div.rtop').css('overflow','auto');
-            for (var key in result_list[number]['vars']) {
-                vars_str += key + ':' + result_list[number]['vars'][key] + ' ';
-            };
             var data_name = $(this).parents('p').find('span[name="name"]').text().trim();
             var data_type = $(this).parents('p').find('span:eq(0)').attr('name').trim();
             var data_content = $(this).parents('p').find('span[name="content"]').text().trim();
@@ -326,7 +323,7 @@
                 $('<li class="nav-item" name="code' + page_num + '"><a class="nav-link" ' +
                     'data-toggle="tab"' +
                     'href="#code' + page_num + '-pan">' +
-                    '<span id="' + page_num + '">' + data_name +
+                    '<span id="' + page_num + '">' + data_label +
                     '</span><button id="close_tab" type="button" ' +
                     'title="Remove this page" name="edit">×</button>' +
                     '</a></li>'));
@@ -335,8 +332,8 @@
                 $('#codeTabContent').append(
                     $('<div style="margin-left:35px;margin-top:20px;" name="' + a_id + '" class="' + class_name + '" id="code' + page_num + '-pan">' +
                         '<label name="' + page_num + '" for="code' + page_num + '"></label> ' +
-                        '<font color="#006000"><b>constant</b></font>:&nbsp;<input spellcheck="false" id="data-name' + page_num + '" style="background:transparent;border:1px;solid #ffffff;width:10%;" value="' + data_name + '">' +
-                        '&nbsp;&nbsp;&nbsp;::&nbsp;&nbsp;&nbsp;<input spellcheck="false" id="data-content' + page_num + '" style="width:50%;background:transparent;border:1px;solid #ffffff;" value="' + data_content + '">' +
+                        '<font color="#006000"><b>constant</b></font>:&nbsp;<input spellcheck="false" id="data-name' + page_num + '" style="background:transparent;'+border+'" value="' + data_name + '">' +
+                        '&nbsp;&nbsp;&nbsp;::&nbsp;&nbsp;&nbsp;<input spellcheck="false" id="data-content' + page_num + '" style="width:50%;background:transparent;'+border+'" value="' + data_content + '">' +
                         '<br><button id="save-edit" name="' + data_type + '" class="el-button el-button--default el-button--mini" style="margin-top:10px;width:20%;"><b>SAVE</b></button></div>'
                         ));
                 $('#codeTab a[href="#code' + page_num + '-pan"]').tab('show');
@@ -345,9 +342,9 @@
                 $('#codeTabContent').append(
                     $('<div style="margin-left:35px;margin-top:20px;" name="' + a_id + '" class="' + class_name + '" id="code' + page_num + '-pan">' +
                         '<label name="' + page_num + '" for="code' + page_num + '"></label> ' +
-                        '<font color="#006000"><b>theorem</b></font>:&nbsp;<input spellcheck="false" id="data-name' + page_num + '" style="margin-top:0px;width:20%;background:transparent;border:1px;solid #ffffff;" value="' + data_name + '">' +
-                        '<br><br>vars:&nbsp;&nbsp;&nbsp;&nbsp;<input spellcheck="false" id="data-vars' + page_num + '" style="width:30%;background:transparent;border:1px;solid #ffffff;" value="' + vars_str + '">' +
-                        '<br><br>term:&nbsp;&nbsp;&nbsp;<input spellcheck="false" id="data-content' + page_num + '" style="width:30%;background:transparent;border:1px;solid #ffffff;" value="' + data_content + '">' +
+                        '<font color="#006000"><b>theorem</b></font>:&nbsp;<input spellcheck="false" id="data-name' + page_num + '" style="margin-top:0px;width:20%;background:transparent;'+border+'" value="' + data_name + '">' +
+                        '<br><br>vars:&nbsp;&nbsp;&nbsp;&nbsp;<input spellcheck="false" id="data-vars' + page_num + '" style="width:30%;background:transparent;'+border+'" value="' + vars_str + '">' +
+                        '<br><br>term:&nbsp;&nbsp;&nbsp;<input spellcheck="false" id="data-content' + page_num + '" style="width:30%;background:transparent;'+border+'" value="' + data_content + '">' +
                         '<br><button id="save-edit" name="' + data_type + '" class="el-button el-button--default el-button--mini" style="margin-top:5px;width:20%;"><b>SAVE</b></button>'+
                         '</br><input name="hint_backward" type="checkbox" style="margin-left:25px;"><b>&nbsp;backward</b><input name="hint_rewrite" style="margin-left:20px;" type="checkbox"><b>&nbsp;rewrite</b></div>'
                         ));
@@ -360,8 +357,8 @@
                 $('#codeTabContent').append(
                     $('<div style="margin-left:35px;margin-top:20px;" name="' + a_id + '" class="' + class_name + '" id="code' + page_num + '-pan">' +
                         '<label name="' + page_num + '" for="code' + page_num + '"><font color="#006000"><b>datatype</b></font>:</label> ' +
-                        '<br><input spellcheck="false" id="data-name' + page_num + '" style="width:10%;background:transparent;border:1px;solid #ffffff;" value="' + data_name + '">' + '&nbsp;&nbsp;=&nbsp;&nbsp;' +
-                        '<br><textarea spellcheck="false" id="data-content' + page_num + '" style="height:60px;width:30%;background:transparent;border:1px;solid #ffffff;">' + data_new_content + '</textarea>' +
+                        '<br><input spellcheck="false" id="data-name' + page_num + '" style="width:10%;background:transparent;'+border+'" value="' + data_name + '">' + '&nbsp;&nbsp;=&nbsp;&nbsp;' +
+                        '<br><textarea spellcheck="false" id="data-content' + page_num + '" style="height:60px;width:30%;background:transparent;'+border+'">' + data_new_content + '</textarea>' +
                             '<br><button id="save-edit" name="' + data_type + '" class="el-button el-button--default el-button--mini" style="margin-top:5px;width:20%;"><b>SAVE</b></button></div>'
                         ));
                 $('#codeTab a[href="#code' + page_num + '-pan"]').tab('show');
@@ -371,23 +368,23 @@
                 var data_new_content = '';
                 for (var i in data_content_list) {
                     data_new_content += i + ': ' + data_content_list[i] + '\n';
-                }
-                ;
-                $('#codeTab').find('span#' + page_num).text(data_name.split(' :: ')[0]);
+                };
+                if (data_name)
+                    $('#codeTab').find('span#' + page_num).text(data_name.split(' :: ')[0]);
                 $('#codeTabContent').append(
                     $('<div style="margin-left:35px;margin-top:20px;" name="' + a_id + '" class="' + class_name + '" id="code' + page_num + '-pan">' +
                         '<label name="' + page_num + '" for="code' + page_num + '"><font color="#006000"><b>fun</b></font>:</label> ' +
-                        '<input spellcheck="false" id="data-name' + page_num + '" style="width:30%;background:transparent;border:1px;solid #ffffff;" value="' + data_name + '">' +
-                        '<br><textarea spellcheck="false" id="data-content' + page_num + '" style="margin-top:5px;height:110px;width:40%;background:transparent;border:0px solid #ffffff;" name="content">' + data_new_content + '</textarea>' +
-                        '&nbsp;&nbsp;for:&nbsp;&nbsp;<textarea spellcheck="false" id="data-vars' + page_num + '" style="margin-top:5px;height:110px;width:40%;background:transparent;border:0px solid #ffffff;" placeholder="vars"></textarea>' +
+                        '<input spellcheck="false" id="data-name' + page_num + '" style="width:30%;background:transparent;'+border+'" value="' + data_name + '">' +
+                        '<br><textarea spellcheck="false" id="data-content' + page_num + '" style="margin-top:5px;height:110px;width:40%;background:transparent;'+border+'" name="content">' + data_new_content + '</textarea>' +
+                        '&nbsp;&nbsp;for:&nbsp;&nbsp;<textarea spellcheck="false" id="data-vars' + page_num + '" style="margin-top:5px;height:110px;width:40%;background:transparent;'+border+'" placeholder="vars"></textarea>' +
                         '<br><button id="save-edit" name="' + data_type + '" class="el-button el-button--default el-button--mini" style="margin-top:5px;width:20%;"><b>SAVE</b></button></div>'
                         ));
                 $('#codeTab a[href="#code' + page_num + '-pan"]').tab('show');
                 display_lines_number(data_content_list, page_num, number);
             }
-            if ('hint_backward' in result_list[number] && result_list[number]['hint_backward'] === 'true')
+            if (number && 'hint_backward' in result_list[number] && result_list[number]['hint_backward'] === 'true')
                 $('input[name="hint_backward"]').click();
-            if ('hint_rewrite' in result_list[number] && result_list[number]['hint_rewrite'] === 'true')
+            if (number && 'hint_rewrite' in result_list[number] && result_list[number]['hint_rewrite'] === 'true')
                 $('input[name="hint_rewrite"]').click();
             $('#codeTabContent div#code' + page_num + '-pan button').after(
                 $('<div class="output-wrapper" style="margi-top:1px;" id="error' + page_num + '">' +
@@ -398,7 +395,6 @@
         function display_lines_number(content_list, page_num, number) {
             var data_vars_list = [];
             var data_vars_str = '';
-<<<<<<< HEAD
             if (number) {
                 $.each(result_list[number]['rules'], function(i, v) {
                     var vars_str = '';
@@ -428,11 +424,11 @@
             var prev_list = result_list.slice(0, number);
             if ($('input[name="hint_backward"]').prop('checked') === true)
                 result_list[number]['hint_backward'] = 'true';
-            else if ('hint_backward' in result_list[number])
+            else if (number!==-1 && 'hint_backward' in result_list[number])
                 delete result_list[number]['hint_backward'];
             if ($('input[name="hint_rewrite"]').prop('checked') === true)
                 result_list[number]['hint_rewrite'] = 'true';
-            else if ('hint_rewrite' in result_list[number])
+            else if (number!==-1 && 'hint_rewrite' in result_list[number])
                 delete result_list[number]['hint_rewrite']
             ajax_data['file-name'] = name;
             ajax_data['prev-list'] = prev_list;
@@ -629,7 +625,6 @@
                 });
             }
         });
-    });
 
     function rp(x) {
         if (x === 0)
