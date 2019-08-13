56e07abf3922fb0c58b83a4f0fe000b4ada15809
willfun <460677873@qq.com>
2019-08-13 21:06:35 +0800

    'new_proof_area'

--- app/Prog-verify/src/components/ProofArea.vue
+++ app/Prog-verify/src/components/ProofArea.vue
@@ -38,11 +38,11 @@ export default {
       if (p[1] === 0) { color = 'color: black' } else if (p[1] === 1) { color = 'color: green' } else if (p[1] === 2) { color = 'color: blue' } else if (p[1] === 3) { color = 'color: purple' } else if (p[1] === 4) { color = 'color: silver' }
       return that.$options.methods.display_str(editor, p[0], lineNo, ch, {css: color})
     },
-    display_highlight_strs(editor, ps, line_no, ch, that) {
+    display_highlight_strs(editor, ps, lineNo, ch, that) {
       for (let i = 0; i < ps.length; i++) {
-          ch = that.$options.methods.display_highlight_str(editor, ps[i], line_no, ch, that);
+        ch = that.$options.methods.display_highlight_str(editor, ps[i], lineNo, ch, that)
       }
-      return ch;
+      return ch
     },
     display_str(editor, str, lineNo, ch, mark) {
       let len = str.length
@@ -52,8 +52,8 @@ export default {
       }
       return ch + len
     },
-    display_line(proof, lineNo) {
-      let editor = this.editor
+    display_line(proof, lineNo, that) {
+      let editor = that.editor
       let line = proof[lineNo]
       let ch = 0
       let strTemp = ''
@@ -62,13 +62,13 @@ export default {
           strTemp += '  '
         }
       }
-      ch = this.$options.methods.display_str(editor, strTemp, lineNo, ch, {css: 'font-weight: bold'})
+      ch = that.$options.methods.display_str(editor, strTemp, lineNo, ch, {css: 'font-weight: bold'})
       if (line.rule === 'assume') {
-        ch = this.$options.methods.display_str(editor, 'assume ', lineNo, ch, {css: 'color: darkcyan; font-weight: bold'})
-        ch = this.$options.methods.display_highlight_strs(editor, line.args_hl, lineNo, ch)
+        ch = that.$options.methods.display_str(editor, 'assume ', lineNo, ch, {css: 'color: darkcyan; font-weight: bold'})
+        ch = that.$options.methods.display_highlight_strs(editor, line.args_hl, lineNo, ch, that)
       } else if (line.rule === 'variable') {
-        ch = this.$options.methods.display_str(editor, 'fix ', lineNo, ch, {css: 'color: darkcyan; font-weight: bold'})
-        ch = this.$options.methods.display_highlight_strs(editor, line.args_hl, lineNo, ch)
+        ch = that.$options.methods.display_str(editor, 'fix ', lineNo, ch, {css: 'color: darkcyan; font-weight: bold'})
+        ch = that.$options.methods.display_highlight_strs(editor, line.args_hl, lineNo, ch, that)
       } else if (line.rule === 'subproof') {
         ch = that.$options.methods.display_have_prompt(editor, proof, lineNo, ch, that)
         ch = that.$options.methods.display_highlight_strs(editor, line.th_hl, lineNo, ch, that)
@@ -81,15 +81,15 @@ export default {
           ch = that.$options.methods.display_str(editor, ' by ', lineNo, ch, {css: 'font-weight: bold'})
         }
         // Display rule name
-        ch = this.$options.methods.display_str(editor, line.rule, lineNo, ch)
+        ch = that.$options.methods.display_str(editor, line.rule, lineNo, ch)
         // Display args with highlight
         if (line.args_hl.length > 0) {
-          ch = this.$options.methods.display_str(editor, ' ', lineNo, ch)
-          ch = this.$options.methods.display_highlight_strs(editor, line.args_hl, lineNo, ch)
+          ch = that.$options.methods.display_str(editor, ' ', lineNo, ch)
+          ch = that.$options.methods.display_highlight_strs(editor, line.args_hl, lineNo, ch, that)
         }
         if (line.prevs.length > 0) {
-          ch = this.$options.methods.display_str(editor, ' from ', lineNo, ch, {css: 'font-weight: bold'})
-          ch = this.$options.methods.display_str(editor, line.prevs.join(', '), lineNo, ch)
+          ch = that.$options.methods.display_str(editor, ' from ', lineNo, ch, {css: 'font-weight: bold'})
+          ch = that.$options.methods.display_str(editor, line.prevs.join(', '), lineNo, ch)
         }
       }
       editor.execCommand('goDocEnd')
@@ -115,7 +115,7 @@ export default {
           if (idLen >= maxIdLen) {
             maxIdLen = idLen
           }
-          that.$options.methods.display_line(proof, lineNo)
+          that.$options.methods.display_line(proof, lineNo, that)
           let len = editor.getLineHandle(lineNo).text.length
           editor.replaceRange('\n', {line: lineNo, ch: len}, {line: lineNo, ch: len + 1})
           if (line.rule === 'intros') {
