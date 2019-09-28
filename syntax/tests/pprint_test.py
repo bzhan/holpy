f8f3d3120aeebfaeeebf076f7e4d618ecb1c5547
Bohua Zhan <bzhan@ios.ac.cn>
2019-09-28 13:04:52 +0800

    Fix unittest.

--- syntax/tests/pprint_test.py
+++ syntax/tests/pprint_test.py
@@ -23,11 +23,11 @@ class PPrintTest(unittest.TestCase):
             [
                 'a ≤ b ∧ ',
                 'continuous_on (λx. f x * g x) (real_closed_interval a b) ∧ ',
-                '(∀x. x ∈ real_closed_interval a b ⟶ ',
-                '   has_v_derivative f (f2 x) (at x) ∧ has_v_derivative g (g2 x) (at x)) ∧ ',
+                '(∀x. x ∈ real_closed_interval a b ',
+                '   ⟶ has_v_derivative f (f2 x) (at x) ∧ has_v_derivative g (g2 x) (at x)) ∧ ',
                 'has_integral (λx. f x * g2 x) (f b * g b - f a * g a - y) ',
-                '  (real_closed_interval a b) ⟶ ',
-                'has_integral (λx. f2 x * g x) y (real_closed_interval a b)'
+                '  (real_closed_interval a b) ',
+                '⟶ has_integral (λx. f2 x * g x) y (real_closed_interval a b)'
             ]
         )
 
@@ -38,9 +38,9 @@ class PPrintTest(unittest.TestCase):
         ast = pprint.get_ast(thy, t, unicode=True)
         res = pprint.print_ast(thy, ast, line_length=20, highlight=True)
         expected_res = [
-            [('long_constant_1 ', 2), ('⟶ ', 0)],
-            [('long_constant_2 ', 2), ('⟶ ', 0)],
-            [('long_constant1 ', 2), ('∧ ', 0)], [('long_constant_2', 2)]
+            [('long_constant_1 ', 2)],
+            [('⟶ ', 0), ('long_constant_2 ', 2)],
+            [('⟶ ', 0), ('long_constant1 ', 2), ('∧ ', 0)], [('long_constant_2', 2)]
         ]
         self.assertEqual(res, expected_res)
 
