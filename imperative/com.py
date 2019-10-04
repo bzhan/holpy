d869592e6aacb24b051d830bf811c565e2cdce98
Bohua Zhan <bzhan@ios.ac.cn>
2019-10-04 20:02:59 +0800

    Merge member functions for different Coms.

--- imperative/com.py
+++ imperative/com.py
@@ -1,6 +1,6 @@
 # Author: Bohua Zhan
 
-"""Basic data structure for programs."""
+"""Basic data structure for commands."""
 
 from kernel.term import Term, Var
 from logic import logic
@@ -9,57 +9,159 @@ from imperative import expr
 
 
 class Com():
-    """Base class for programs."""
+    """Base class for commands.
+    
+    There are five basic types of commands: Skip, Assign, Seq, Cond
+    and While, represented by sub-classes to be defined below.
+    
+    """
     def __init__(self):
+        """Each command is associated with a sequence of preconditions
+        and a sequence of postconditions. Let the preconditions be
+        P_1, ..., P_n and the postconditions be Q_1, ..., Q_n, then
+        <P_n> c <Q_1> is the Hoare triple derived directly from the
+        Hoare rules, <P_1> c <Q_n> is the Hoare triple exposed to the
+        outside, and P_1 --> P_2, ... and Q_1 --> Q_2, ... are the
+        verification conditions.
+
+        """
         self.pre = []
         self.post = []
 
     def get_vc_pre(self):
+        """Return the verification conditions corresponding to the
+        preconditions.
+
+        """
         res = []
         for i in range(len(self.pre) - 1):
             res.append(expr.implies(self.pre[i], self.pre[i+1]))
         return res
 
     def print_vc_pre(self):
+        """Print the verification conditions corresponding to the
+        preconditions.
+
+        """
         res = ""
         for t in self.get_vc_pre():
             res = res + "<" + str(t) + ">\n"
         return res
 
     def get_vc_post(self):
+        """Return the verification conditions corresponding to the
+        postconditions.
+
+        """
         res = []
         for i in range(len(self.post) - 1):
             res.append(expr.implies(self.post[i], self.post[i+1]))
         return res
 
     def print_vc_post(self):
+        """Print the verification conditions corresponding to the
+        postconditions.
+
+        """
         res = ""
         for t in self.get_vc_post():
             res = res + "\n<" + str(t) + ">"
         return res
 
     def compute_wp(self, post):
-        """Given post-condition for a command, find the pre-condition
-        and verification conditions.
+        """Given postcondition for a command, find the weakest
+        precondition (wp) and verification conditions. Returns
+        the weakest precondition for the command.
 
         """
-        raise NotImplementedError
+        if isinstance(self, Skip):
+            # <Q> Skip <Q>
+            self.post = [post]
+            self.pre.append(post)
+
+        elif isinstance(self, Assign):
+            if isinstance(self.v, expr.Var):
+                # <Q[x := e]> x := e <Q>
+                self.post = [post]
+                self.pre.append(post.subst({self.v.name: self.e}))
+            else:
+                raise NotImplementedError
+
+        elif isinstance(self, Seq):
+            #  <P> c1 <R>
+            #  <R> c2 <Q>
+            # --------------
+            # <P> c1; c2 <Q>
+            self.post = [post]
+            mid = self.c2.compute_wp(post)
+            pre = self.c1.compute_wp(mid)
+            self.pre.append(pre)
+
+        elif isinstance(self, Cond):
+            #    <P1> c1 <Q>
+            #    <P2> c2 <Q>
+            # -----------------------------------------------
+            # <if b then P1 else P2> if b then c1 else c2 <Q>
+            self.post = [post]
+            pre_c1 = self.c1.compute_wp(post)
+            pre_c2 = self.c2.compute_wp(post)
+            self.pre.append(expr.ITE(self.b, pre_c1, pre_c2))
+
+        elif isinstance(self, While):
+            # Original rule is:
+            #  <I & b> c <I>
+            # -----------------
+            # <I> while b do c <I & ~b>
+            # From this, can derive:
+            #  <I & b> c <I>
+            #  I & ~b --> Q
+            # -----------------
+            # <I> while b do c <Q>
+            self.pre.append(self.inv)
+            self.c.pre = [expr.conj(self.inv, self.b)] + self.c.pre
+            self.c.compute_wp(self.inv)
+            self.post = [expr.conj(self.inv, expr.neg(self.b)), post]
+
+        else:
+            raise TypeError
+
+        return self.pre[0]
+
+    def print_com(self, thy):
+        if isinstance(self, Skip):
+            return self.print_vc_pre() + "skip"
+        elif isinstance(self, Assign):
+            return self.print_vc_pre() + str(self.v) + " := " + str(self.e)
+        elif isinstance(self, Seq):
+            return self.print_vc_pre() + \
+                self.c1.print_com(thy) + ";\n" + self.c2.print_com(thy)
+        elif isinstance(self, Cond):
+            return self.print_vc_pre() + \
+                "if (%s) then\n  %s\nelse\n  %s" % (
+                str(self.b), self.c1.print_com(thy), self.c2.print_com(thy))
+        elif isinstance(self, While):
+            cmd = self.c.print_com(thy).split('\n')
+            cmd = '\n  '.join(cmd)
+            return self.print_vc_pre() + "while (%s) {\n  [%s]\n  %s\n}" % (
+                str(self.b), str(self.inv), cmd) + self.print_vc_post()
+        else:
+            raise TypeError
+
+    def get_vc(self):
+        if isinstance(self, (Skip, Assign)):
+            return self.get_vc_pre()
+        elif isinstance(self, (Seq, Cond)):
+            return self.get_vc_pre() + self.c1.get_vc() + self.c2.get_vc()
+        elif isinstance(self, While):
+            return self.get_vc_pre() + self.c.get_vc() + self.get_vc_post()
+        else:
+            raise TypeError
+
 
 class Skip(Com):
     """Skip program."""
     def __init__(self):
         super().__init__()
-
-    def print_com(self, thy):
-        return self.print_vc_pre() + "skip"
-
-    def get_vc(self):
-        return self.get_vc_pre()
-
-    def compute_wp(self, post):
-        self.post = [post]
-        self.pre.append(post)
-        return self.pre[0]
 
 class Assign(Com):
     """Assign program."""
@@ -71,20 +173,6 @@ class Assign(Com):
         self.v = v
         self.e = e
 
-    def print_com(self, thy):
-        return self.print_vc_pre() + str(self.v) + " := " + str(self.e)
-
-    def get_vc(self):
-        return self.get_vc_pre()
-
-    def compute_wp(self, post):
-        if isinstance(self.v, expr.Var):
-            self.post = [post]
-            self.pre.append(post.subst({self.v.name: self.e}))
-            return self.pre[0]
-        else:
-            raise NotImplementedError
-
 class Seq(Com):
     """Sequence program."""
     def __init__(self, c1, c2):
@@ -92,20 +180,6 @@ class Seq(Com):
         super().__init__()
         self.c1 = c1
         self.c2 = c2
-
-    def print_com(self, thy):
-        return self.print_vc_pre() + \
-            self.c1.print_com(thy) + ";\n" + self.c2.print_com(thy)
-
-    def get_vc(self):
-        return self.get_vc_pre() + self.c1.get_vc() + self.c2.get_vc()
-
-    def compute_wp(self, post):
-        self.post = [post]
-        mid = self.c2.compute_wp(post)
-        pre = self.c1.compute_wp(mid)
-        self.pre.append(pre)
-        return self.pre[0]
 
 class Cond(Com):
     """Conditional program."""
@@ -116,21 +190,6 @@ class Cond(Com):
         self.c1 = c1
         self.c2 = c2
 
-    def compute_wp(self, post):
-        self.post = [post]
-        pre_c1 = self.c1.compute_wp(post)
-        pre_c2 = self.c2.compute_wp(post)
-        self.pre.append(expr.ITE(self.b, pre_c1, pre_c2))
-        return self.pre[0]
-
-    def get_vc(self):
-        return self.get_vc_pre() + self.c1.get_vc() + self.c2.get_vc()
-
-    def print_com(self, thy):
-        return self.print_vc_pre() + \
-            "if (%s) then\n  %s\nelse\n  %s" % (
-            str(self.b), self.c1.print_com(thy), self.c2.print_com(thy))
-
 class While(Com):
     """While program."""
     def __init__(self, b, inv, c):
@@ -139,19 +198,3 @@ class While(Com):
         self.b = b
         self.inv = inv
         self.c = c
-
-    def compute_wp(self, post):
-        self.pre.append(self.inv)
-        self.c.pre = [expr.conj(self.inv, self.b)] + self.c.pre
-        self.c.compute_wp(self.inv)
-        self.post = [expr.conj(self.inv, expr.neg(self.b)), post]
-        return self.pre[0]
-
-    def get_vc(self):
-        return self.get_vc_pre() + self.c.get_vc() + self.get_vc_post()
-
-    def print_com(self, thy):
-        cmd = self.c.print_com(thy).split('\n')
-        cmd = '\n  '.join(cmd)
-        return self.print_vc_pre() + "while (%s) {\n  [%s]\n  %s\n}" % (
-            str(self.b), str(self.inv), cmd) + self.print_vc_post()
