1f0f91fa0a6776face0b0dd7924d6ca7e84c3e02
bhli1999 <li001678@umn.edu>
2020-01-28 18:46:54 +0800

    update app/src/router.js. Update Router to combine geometry to the menu

--- app/src/router.js
+++ app/src/router.js
@@ -10,7 +10,7 @@ import Editor from './components/Editor.vue'
 import ProVerify from './components/ProVerify.vue'
 import Monitor from './components/Monitor.vue'
 import Integral from './components/Integral.vue'
-
+import Geometry from './components/geometry/Geometry.vue'
 const routes = [
   {path: '/', name: 'main', component: Index},
   {path: '/login', name: 'login', component: Login},
@@ -19,6 +19,7 @@ const routes = [
   {path: '/program', name: 'program', component: ProVerify},
   {path: '/integral', name: 'integral', component: Integral},
   {path: '/monitor', name: 'monitor', component: Monitor}
+  {path: '/geometry', name: 'geometry', component: Geometry}
 ]
 
 const router = new VueRouter({
