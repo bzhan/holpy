c2c7217e68201462473bb32421550bd5980b9533
Bohua Zhan <bzhan@ios.ac.cn>
2020-01-29 14:52:59 +0800

    Add examples.

--- integral/tests/run_integral.py
+++ integral/tests/run_integral.py
@@ -113,6 +113,19 @@ test_cases = {
         # "Exercise 12": None,
         # "Exercise 13": None,
         # "Exercise 14": "0",
+        "Exercise 15": "-exp(4) + exp(5)",
+        "Exercise 16": "0",
+        # "Exercise 17": "-(1/2) * log(2) ^ 2 + 1/2 * log(2 ^ (1/2) * (1/2)) ^ 2",
+        # "Exercise 18": None,
+        "Exercise 19": "-1/2 + 2 ^ (1/2) * (1/2)",
+        "Exercise 20": "-1/9",
+        "Exercise 21": "-2 + 1/4 * pi^2",
+        "Exercise 22": "1 + (-1 + 2 ^ (1/2) * (1/2)) * exp(2 ^ (1/2) * (1/2))",
+        "Exercise 23": "1/2 + 1/2 * exp(1/2 * pi)",
+        "Exercise 24": "-3/7",
+        # "Exercise 25": None,
+        # "Exercise 26": "0",
+        # "Exercise 27": "log(1 + exp(1/2 * pi))",
     }
 }
 
