7941c04bafa60ebd3346c708eb24e60a02c21796
Chengwei Zhang <1535909927@qq.com>
2020-02-16 21:05:13 +0800

    add Point component.

--- app/src/components/geometry/Stage.vue
+++ app/src/components/geometry/Stage.vue
@@ -1,28 +1,27 @@
 <template>
     <v-stage :config="configKonva">
         <v-layer>
-            <v-circle :config="configCircle"></v-circle>
+           <Point :x=200 :y=300 :id="pointText"></Point>
         </v-layer>
     </v-stage>
 </template>
 
 <script>
+const width = window.innerWidth
+const height = window.innerHeight
+import Point from "./Point"
 export default {
     name: 'Stage',
+    components: {
+        Point
+    },
     data() {
         return {
             configKonva: {
-                width: 200,
-                height: 200
+                width: width,
+                height: height
             },
-            configCircle: {
-                x: 100,
-                y: 100,
-                radius: 70,
-                fill: "red",
-                stroke: "black",
-                strokeWidth: 4
-            }
+            pointText: "A",
         };
     }
 };
