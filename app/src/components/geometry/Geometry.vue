ac077f3ba235f61f948bcb5ecf72e734ab94c5e4
Chengwei Zhang <1535909927@qq.com>
2020-02-17 12:52:21 +0800

    add tool area & remove useless area.

--- app/src/components/geometry/Geometry.vue
+++ app/src/components/geometry/Geometry.vue
@@ -5,48 +5,59 @@
       <b-navbar-brand href="#">geometry</b-navbar-brand>
       <b-navbar-nav>
         <b-nav-item-dropdown text="File" left>
-          <b-dropdown-item href="#" v-on:click='new_file'>New</b-dropdown-item>
-          <b-dropdown-item href="#" v-on:click='open_file'>Open</b-dropdown-item>
-          <b-dropdown-item href="#" v-on:click='load_file'>Save</b-dropdown-item>
+          <b-dropdown-item href="#">New</b-dropdown-item>
+          <b-dropdown-item href="#">Open</b-dropdown-item>
+          <b-dropdown-item href="#">Save</b-dropdown-item>
         </b-nav-item-dropdown>
+        <b-nav-item href="#" @click="handleClickSelect">Select</b-nav-item>
         <b-nav-item-dropdown text="Construct" left>
-          <b-dropdown-item href="#" v-on:click='no_action'>Point</b-dropdown-item>
-          <b-dropdown-item href="#" v-on:click='no_action'>Line</b-dropdown-item>
-          <b-dropdown-item href="#" v-on:click='no_action'>Circle</b-dropdown-item>
+          <b-dropdown-item href="#" @click="handleClickPoint">Point</b-dropdown-item>
+          <b-dropdown-item href="#">Line</b-dropdown-item>
+          <b-dropdown-item href="#">Circle</b-dropdown-item>
         </b-nav-item-dropdown>
         <b-nav-item-dropdown text="Constraint" left>
-          <b-dropdown-item href="#" v-on:click='no_action'>Equal Angle</b-dropdown-item>
-          <b-dropdown-item href="#" v-on:click='no_action'>Equal Ratio</b-dropdown-item>
+          <b-dropdown-item href="#">Equal Angle</b-dropdown-item>
+          <b-dropdown-item href="#">Equal Ratio</b-dropdown-item>
         </b-nav-item-dropdown>
         <b-nav-item-dropdown text="Prove" left>
-          <b-dropdown-item href="#" v-on:click='no_action'>Prove</b-dropdown-item>          
+          <b-dropdown-item href="#">Prove</b-dropdown-item>
         </b-nav-item-dropdown>
         <b-nav-item-dropdown text="Help" left>
-          <b-dropdown-item href='#' v-on:click='no_action'>Help</b-dropdown-item>
+          <b-dropdown-item href='#'>Help</b-dropdown-item>
         </b-nav-item-dropdown>        
       </b-navbar-nav>
     </b-navbar>
+    <div id="canvas" ref="p">
+      <v-stage :config="stageSize" ref="stage" @click="handleClickOnLayer">
+        <v-layer id="defaultLayer" ref="layer">
+          <Point :x=100 :y=100 :id="'A'"></Point>
+        </v-layer>
+      </v-stage>
+    </div>
+    <div id="tool">
+      <h6>{{status}}</h6>
+    </div>
 
-    <div id="theory-list" v-show="ref_proof === undefined">
-      <Content v-if="filelist !== undefined"
-               v-bind:filelist="filelist"
-               v-on:select-theory="onSelectTheory"
-               ref="content"/>
-    </div>
-    <div id="proof-context" v-show="ref_proof !== undefined">
-      <ProofContext v-bind:ref_proof="ref_proof" ref="context"/>
-    </div>
-    <div id="theory-content">
-      <Theory v-bind:theory="theory"
-              v-bind:ref_status="ref_status"
-              v-bind:ref_context="ref_context"
-              :editor="editor"
-              v-on:set-message="onSetMessage"
-              v-on:set-proof="handle_set_proof"
-              v-on:query="handle_query"
-              v-on:goto-link="handleGoToLink"
-              ref="theory"/>
-    </div>
+<!--    <div id="theory-list" v-show="ref_proof === undefined">-->
+<!--      <Content v-if="filelist !== undefined"-->
+<!--               v-bind:filelist="filelist"-->
+<!--               v-on:select-theory="onSelectTheory"-->
+<!--               ref="content"/>-->
+<!--    </div>-->
+<!--    <div id="proof-context" v-show="ref_proof !== undefined">-->
+<!--      <ProofContext v-bind:ref_proof="ref_proof" ref="context"/>-->
+<!--    </div>-->
+<!--    <div id="theory-content">-->
+<!--      <Theory v-bind:theory="theory"-->
+<!--              v-bind:ref_status="ref_status"-->
+<!--              v-bind:ref_context="ref_context"-->
+<!--              :editor="editor"-->
+<!--              v-on:set-message="onSetMessage"-->
+<!--              v-on:set-proof="handle_set_proof"-->
+<!--              v-on:query="handle_query"-->
+<!--              v-on:goto-link="handleGoToLink"-->
+<!--              ref="theory"/>-->
+<!--    </div>-->
 <!--    <div id="message" v-show="ref_proof === undefined">-->
 <!--      <Message v-bind:message="message" ref="message"/>-->
 <!--    </div>-->
@@ -58,13 +69,7 @@
 <!--                  v-on:query-ok="handle_query_ok"-->
 <!--                  v-on:query-cancel="handle_query_cancel"/>-->
 <!--    </div>-->
-    <div id="paint" ref="p">
-      <v-stage :config="stageSize" ref="stage" @click="handleClickOnLayer">
-        <v-layer id="defaultLayer" ref="layer">
-          <Point :x=100 :y=100 :id="'A'"></Point>
-        </v-layer>
-      </v-stage>
-    </div>
+
   </div>
 </template>
 
@@ -86,6 +91,7 @@
           width: null,
           height: null,
         },
+        status: "select",
       }
     },
     mounted() {
@@ -101,17 +107,26 @@
         this.stageSize.width = this.$refs.p.clientWidth - 10
       },
       handleClickOnLayer() {
-        const newShape = new Konva.Circle({
-          x: this.$refs.stage.getNode().getPointerPosition().x,
-          y: this.$refs.stage.getNode().getPointerPosition().y,
-          radius: 5,
-          stroke: "black",
-          strokeWidth: 2,
-          fill: Konva.Util.getRandomColor(),
-        });
-        this.$refs.layer.getNode().add(newShape);
-        this.$refs.layer.getNode().draw();
+        if (this.status === "point") {
+          const newShape = new Konva.Circle({
+            x: this.$refs.stage.getNode().getPointerPosition().x,
+            y: this.$refs.stage.getNode().getPointerPosition().y,
+            radius: 5,
+            stroke: "black",
+            strokeWidth: 2,
+            fill: "red",
+          });
+          this.$refs.layer.getNode().add(newShape);
+          this.$refs.layer.getNode().draw();
+        }
+      },
+      handleClickSelect() {
+        this.status = "select"
+      },
+      handleClickPoint() {
+        this.status = "point"
       }
+
 
     }
   }
@@ -123,7 +138,7 @@
 /*::-webkit-scrollbar {*/
 /*  width: 0 !important;height: 0;}*/
 
-#paint {
+#canvas {
   display: inline-block;
   width: 75%;
   position: fixed;
@@ -162,7 +177,21 @@
 #message, #status, #query {
   display: inline-block;
   width: 75%;
-  height: 30%;
+  height: 25%;
+  left: 25%;
+  position: fixed;
+  top: 75%;
+  bottom: 0;
+  padding-left: 10px;
+  padding-top: 10px;
+  border-top-style: solid;
+  overflow-y: scroll;
+}
+
+#tool {
+  display: inline-block;
+  width: 75%;
+  height: 5%;
   left: 25%;
   position: fixed;
   top: 70%;
