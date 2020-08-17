36f2e0a8482cb18d4e79a1b41a972c9b2e669e8f
rqxu <xrunqing@outlook.com>
2020-08-17 19:58:19 +0800

    Add some macros.

--- data/integer.py
+++ data/integer.py
@@ -3,15 +3,18 @@ from kernel.term import Var, Int, Eq, Term, equals, Const, less, less_eq, greate
 from kernel import term_ord
 from kernel.proofterm import refl
 from kernel.macro import Macro
-from kernel.theory import check_proof, register_macro
+from kernel.theory import check_proof, register_macro, get_theorem
 from kernel.proofterm import ProofTerm
 from kernel.report import ProofReport
 from logic import basic
 from logic import context
+from logic.logic import apply_theorem
 from logic.conv import Conv, rewr_conv, arg_conv, arg1_conv, binop_conv, top_conv, ConvException, try_conv
 from data import nat
 from kernel.thm import Thm
 from syntax.settings import settings
+from math import gcd
+from logic import matcher
 basic.load_theory('int')
 
 
@@ -48,12 +51,28 @@ def int_eval(t):
     else:
         raise ConvException('int_eval: %s' % str(t))
 
+@register_macro('int_eval')
+class int_eval_macro(Macro):
+    """Simplify integer expression"""
+    def __init__(self):
+        self.level = 0 # no expand implement
+        self.sig = Term
+        self.limit = None
+
+    def eval(self, goal, prevs):
+        assert len(prevs) == 0, "int_eval_macro: no conditions expected"
+        assert goal.is_equals(), "int_eval_macro: goal must be an equality"
+        assert int_eval(goal.lhs) == int_eval(goal.rhs), "int_eval_macro: two sides are not equal"
+
+        return Thm([], goal)
+
 class int_conv(Conv):
-    def eval(self, t):
-        return Thm([], Eq(t, int_eval(t)))
-    
     def get_proof_term(self, t):
-        return ProofTerm.sorry(Thm([], Eq(t, int_eval(t))))
+        simp_t = Int(int_eval(t))
+        if simp_t == t:
+            return refl(t)
+        else:
+            return ProofTerm('int_eval', Eq(t, int_eval(t)))
 
 class norm_mult_atom(Conv):
     def get_proof_term(self, t):
@@ -136,13 +155,6 @@ class norm_add_monomial(Conv):
     def get_proof_term(self, t):
         pt = refl(t)
         if t.is_minus(): # a - c
-            # if t.arg.arg1.dest_number() < 0: # a - (-k) * body, k > 0
-            #     return pt.on_rhs(
-            #         arg_conv(rewr_conv('mul_opp_l')), # a - (-(k * body))
-            #         rewr_conv('sub_opp_r'), # a + k * body
-            #         arg_conv(norm_mult_monomial()),
-            #         norm_add_monomial())
-            # else:
             return pt.on_rhs(
                 rewr_conv('int_poly_neg2'), # a + (-k) * body
                 arg_conv(norm_mult_monomial()),
@@ -165,6 +177,10 @@ class norm_add_monomial(Conv):
                 return pt
         
         else:  # a + b
+            if t.arg.is_zero():
+                return pt.on_rhs(rewr_conv('int_add_0_right'))
+            if t.arg1.is_zero():
+                return pt.on_rhs(rewr_conv('int_add_0_left'))
             cp = compare_monomial(t.arg1, t.arg)  # compare a with b
             if cp > 0:  # if a > b, need to swap a with b
                 return pt.on_rhs(rewr_conv('int_add_comm'))
@@ -350,3 +366,161 @@ class int_eq_macro(Macro):
         assert pt1.rhs == pt2.rhs
         return pt1.transitive(pt2.symmetric())
 
+@register_macro('int_ineq')
+class int_ineq_macro(Macro):
+    """
+    Convert all kinds of inequalities to less equalities.
+    Method: 
+    1) First move all terms to lhs, normalize lhs
+    2) c * x + ⋯ < 0, no conversion;
+    3) c * x + ⋯ ≤ 0 --> c * x + ⋯ < 1;
+    3) c * x + ⋯ > 0 --> (-c) * x + ⋯ < 0;
+    4) c * x + ⋯ ≥ 0 --> (-c) * x + ⋯ < 1;
+    """
+    def get_proof_term(self, goal):
+        assert isinstance(goal, Term), "%s should be a hol term" % str(goal)
+        assert goal.is_less() or goal.is_less_eq() or goal.is_greater() or goal.is_greater_eq(),\
+            "%s should be an inequality term" % str(goal)
+
+        norm_ineq_pt = norm_eq().get_proof_term(goal)
+        # find the first monomial
+        first_monomial = norm_ineq_pt.rhs
+        while not first_monomial.is_times() and not first_monomial.is_number():
+            first_monomial = first_monomial.arg1
+
+        coeff = first_monomial.arg1 if first_monomial.is_times() else first_monomial
+        assert coeff.is_int()
+        coeff_value = int_eval(coeff)
+        # normalize
+        if norm_ineq_pt.rhs.is_less():
+            return norm_ineq_pt
+        elif norm_ineq_pt.rhs.is_less_eq():
+            return norm_ineq_pt.on_rhs(rewr_conv('lesseq_0'))
+        else:
+            if norm_ineq_pt.rhs.is_greater():
+                pt_less = norm_ineq_pt.on_rhs(rewr_conv('greater_less'))
+            elif norm_ineq_pt.rhs.is_greater_eq():
+                pt_less = norm_ineq_pt.on_rhs(rewr_conv('greatereq_less'))
+            pt_norm_lhs = refl(pt_less.rhs.arg1).on_rhs(norm_full())
+            return pt_less.transitive(refl(pt_less.rhs.head).combination(pt_norm_lhs).combination(refl(pt_less.rhs.arg)))
+
+@register_macro('int_ineq_mul_const')
+class int_ineq_mul_const_macro(Macro):
+    """
+    Multiply a constant on both side of an inequality.
+
+    prevs is a list contain two proof term:
+    1) m ⋈ n
+    2) c ⋈ 0
+
+    return a proof term like: c * m ⋈ c * n
+    """
+    def get_proof_term(self, prevs, args):
+        # assert isinstance(prevs, list) and len(prevs) == 2 and \
+        #     all(isinstance(pt, ProofTerm) for pt in prevs), "int_ineq_mul_const_macro"
+        # assert prevs[1].prop.arg1.is_int() and prevs[1].prop.arg.is_zero(), "Unexpected %s" % str(prevs[1])
+        assert isinstance(prevs, ProofTerm) and prevs.prop.arg1.is_int() and prevs.prop.arg.is_zero(), "Unexpected %s" % str(prevs)
+        assert isinstance(args, Term) and (args.is_less() or args.is_less_eq() or args.is_greater or args.is_greater_eq())
+        th_names = ['pos_mul_less', 'neg_mul_less', 'pos_mul_less_eq', 'neg_mul_less_eq',
+                    'pos_mul_greater', 'neg_mul_greater', 'pos_mul_greater_eq', 'neg_mul_greater_eq']
+        for th in th_names:
+            try:
+                th1 = get_theorem(th)
+                inst = matcher.first_order_match(th1.prop.arg.lhs, args)
+                pt_concl = apply_theorem(th, prevs, inst=inst)
+                return pt_concl
+            except:
+                continue
+
+        raise NotImplementedError
+
+def collect_int_polynomial_coeff(poly):
+    """Give an polynomial p, normalize it
+    return a list of triple: (coeff, var_name, power)
+    """
+    assert poly.get_type() == IntType, "%s should be an integer term" % str(poly)
+    p = norm_full().get_proof_term(poly).rhs
+    triple = []
+    while p.is_plus() or p.is_times():
+        t = p.arg if p.is_plus() else p
+        if t.is_number():
+            triple.append((int_eval(t), '', 0))
+        else:
+            triple.append((int_eval(t.arg1), t.arg.arg1.name, nat.nat_eval(t.arg.arg)))
+        p = p.arg1
+    
+    return tuple(reversed(triple))
+
+
+@register_macro('int_const_ineq')
+class int_const_ineq_macro(Macro):
+    """Get an pure integer inequality"""
+    def __init__(self):
+        self.level = 0 # no expand implement
+        self.sig = Term
+        self.limit = None
+
+    def eval(self, goal, prevs):
+        assert len(prevs) == 0, "int_eval_macro: no conditions expected"
+        assert goal.is_less() or goal.is_greater() or goal.is_less_eq() or goal.is_greater_eq(),\
+            "int_eval_macro: goal must be an inequality"
+        lhs, rhs = int_eval(goal.arg1), int_eval(goal.arg)
+        if goal.is_less():
+            assert lhs < rhs
+        if goal.is_less_eq():
+            assert lhs <= rhs
+        if goal.is_greater():
+            assert lhs > rhs
+        if goal.is_greater_eq():
+            assert lhs >= rhs
+
+        return Thm([], goal)
+
+
+@register_macro('int_multiple_ineq_equiv')
+class int_multiple_ineq_equiv(Macro):
+    """
+    Give two inequalities:
+    1) c1 * m ⋈ c1 * n
+    2) c2 * m ⋈ c2 * n
+    prove their equaivalence.
+    """
+    def get_proof_term(self, prevs):
+        p1, p2 = prevs
+        lhs_triple = collect_int_polynomial_coeff(p1.arg1)
+        rhs_triple = collect_int_polynomial_coeff(p2.arg1)
+        lhs_singleton = [(p[1], p[2]) for p in lhs_triple]
+        rhs_singleton = [(p[1], p[2]) for p in rhs_triple]
+        if lhs_singleton != rhs_singleton or len(lhs_singleton) != len(rhs_singleton):
+            raise NotImplementedError
+
+        lhs_coeff = [p[0] for p in lhs_triple]
+        rhs_coeff = [p[0] for p in rhs_triple]
+
+        ratios = [p1/p2 for p1, p2 in zip(lhs_coeff, rhs_coeff)]
+        if len(set(ratios)) != 1:
+            raise NotImplementedError
+        
+        lhs_mul = int(rhs_coeff[0] / gcd(lhs_coeff[0], rhs_coeff[0]))
+        rhs_mul = int(lhs_coeff[0] / gcd(lhs_coeff[0], rhs_coeff[0]))
+
+        if lhs_mul > 0:
+            pt_lhs_mul = ProofTerm('int_const_ineq', greater(IntType)(Int(lhs_mul), Int(0)))
+
+        if lhs_mul < 0:
+            pt_lhs_mul = ProofTerm('int_const_ineq', less(IntType)(Int(lhs_mul), Int(0)))
+
+        if rhs_mul > 0:
+            pt_rhs_mul = ProofTerm('int_const_ineq', greater(IntType)(Int(rhs_mul), Int(0)))
+
+        if rhs_mul < 0:
+            pt_rhs_mul = ProofTerm('int_const_ineq', less(IntType)(Int(rhs_mul), Int(0)))
+
+        pt_lhs_mul = int_ineq_mul_const_macro().get_proof_term(pt_lhs_mul, p1)
+        pt_rhs_mul = int_ineq_mul_const_macro().get_proof_term(pt_rhs_mul, p2)
+
+        # normalize both sides
+        pt_lhs_mul_norm = pt_lhs_mul.transitive(norm_eq().get_proof_term(pt_lhs_mul.prop.rhs))
+        pt_rhs_mul_norm = pt_rhs_mul.transitive(norm_eq().get_proof_term(pt_rhs_mul.prop.rhs))
+
+        return pt_lhs_mul_norm.transitive(pt_rhs_mul_norm.symmetric())
