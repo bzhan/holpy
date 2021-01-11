d3a4288f6a217aa56ef5bc3a907d4e6083889ed1
rqxu <xurq@ios.ac.cn>
2021-01-11 17:56:23 +0800

    Fix bugs.

--- smt/veriT/parser.py
+++ smt/veriT/parser.py
@@ -6,21 +6,21 @@ A proof parser for veriT with arguments:
 from lark import Lark, Transformer, v_args, exceptions
 from smt.veriT.proof import *
 from kernel.term import *
+from kernel.type import TConst
+from kernel.proofterm import ProofTerm
 
 grammar = r"""
-    ?sort_type: "(declare-sort " CNAME INT ")"
-    ?fun_type: "(declare-fun " CNAME (CNAME*) CNAME ")"
-
-    ?type: sort_type -> type | fun_type -> type
-
-    ?atom: CNAME -> var_name
+    ?type: "(declare-sort" NAME INT ")" -> sort_type
+        | "(declare-fun " NAME "(" NAME+ ")" NAME ")" -> fun_type1
+        | "(declare-fun " NAME "()" NAME ")" -> fun_type2
+    ?atom: NAME -> var_name
         | INT -> integer
         | DECIMAL -> decimal
-        | "@" CNAME -> quant_var
+        | "@" NAME -> quant_var
         
-    ?typed_atom: "(" atom "Int" ")" -> int_tm
-        | "(" atom "Real)" -> real_tm
-        | "(" atom "Bool)" -> bool_tm 
+    ?typed_atom: "(" NAME "Int" ")" -> int_tm
+        | "(" NAME "Real)" -> real_tm
+        | "(" NAME "Bool)" -> bool_tm 
     
     ?comb: "(" comb atom+ ")" -> comb_tm
         | atom
@@ -39,36 +39,94 @@ grammar = r"""
         | arith
 
     ?logical: "(not" logical ")" -> neg_tm
-        | "(and" logical logical+ ")" -> and_tm
-        | "(or" logical logical+ ")" -> or_tm
-        | "(=>" logical logical ")" -> implies_tm
-        | "#" INT ":" logical -> name_tm
+        | "(and" logical logical+ ")" -> conj_tm
+        | "(or" logical logical+ ")" -> disj_tm
+        | "(=>" logical logical ")" -> implies_tm        
+        | "#" INT ":" logical -> names_tm
         | "#" INT -> repr_tm
-        | "(exists" "(" typed_atom+ ")" logical ")" -> exists_tm
-        | "(forall" "(" typed_atom+ ")" logical ")" -> forall_tm
-        | "(let (" logical+ "))" -> let_tm
+        | "(exists" "(" typed_atom+ ")" logical* ")" -> exists_tm
+        | "(forall" "(" typed_atom+ ")" logical* ")" -> forall_tm
+        | "(" logical logical ")" -> pair_tm
+        | "(=" logical logical ")" -> equals_tm
+        | "#" INT ":(let (" (logical)+ ")" logical* ")" -> name_let_tm
         | compares
 
-    ?conclusion: "conclusion (" logical* ")" -> concl_tm | "conclusion ((" logical* "))" -> concl_tm
+    ?conclusion: "conclusion (" logical* ")" -> concl_tm
 
-    ?clause_name: "c" INT -> clause_name
+    ?clause_name: ".c" INT -> clause_name
 
-    ?clauses: "clauses (." clause_name+ ")" -> clauses
+    ?clauses: "clauses (" clause_name+ ")" -> clauses
 
-    ?args: "args (" CNAME+ ")" -> args
+    ?args: "args (" CNAME ")" -> args
 
-    ?step: "(set .c" INT "(" CNAME ":" (clauses | args) ":" conclusion "))" -> step
-
-    ?input: "(set .c" INT "(" CNAME ":" conclusion "))" -> input
-    
-    ?proof: step -> proof | input -> proof
+    ?proof: "(set .c" INT "(" CNAME ":" ((clauses | args)":")? conclusion "))" -> step_proof 
+        | "(set .c" INT "(input :" conclusion "))" -> input_proof
+        | logical
 
     %import common.CNAME
     %import common.INT
+    %import common.DIGIT
     %import common.DECIMAL
     %import common.WS
     %ignore WS
+    NAME: (CNAME|"$"|"@")("$"|"@"|CNAME|DIGIT)*
 """
+@v_args(inline=True)
+class TypeTransformer(Transformer):
+    """Parse types in format of smt2."""
+    def __init__(self):
+        pass
+
+    def sort_type(self, name, arity):
+        return TConst(str(name))
+
+    def fun_type1(self, name, *args):
+        """
+        Args:
+            n1: name of the variable
+            n2: list of arguments
+            n3: range type
+
+        return a HOL variable
+        """ 
+        domain_type = [TConst(t) for t in args[:-1]]
+        if args[-1] == "Bool":
+            range_type = BoolType
+        elif args[-1] == "Int":
+            range_type = IntType
+        elif args[-1] == "Real":
+            range_type = RealType
+        else:
+            range_type = TConst(args[-1])
+
+        return Var(str(name), TFun(*domain_type, range_type))
+
+    def fun_type2(self, n1, n2):
+        """
+        Args:
+            n1: name of the variable
+            n2: list of arguments
+            n3: range type
+
+        return a HOL variable
+        """ 
+        if n2 == "Bool":
+            range_type = BoolType
+        elif n2 == "Int":
+            range_type = IntType
+        elif n2 == "Real":
+            range_type = RealType
+        else:
+            range_type = TConst(n3)
+
+        return Var(str(n1), range_type)
+
+def bind_var(smt2_file):
+    """Given a smt2 file, parse the declaration of sorts and return a dict."""
+    with open(smt2_file, "r") as f:
+        return {type_parser.parse(s.replace("\n", "")).name: \
+                    type_parser.parse(s.replace("\n", "")) for s in f.readlines() if \
+                        s.startswith("(declare-fun")}
 
 @v_args(inline=True)
 class TermTransformer(Transformer):
@@ -76,12 +134,18 @@ class TermTransformer(Transformer):
     def __init__(self, sorts):
         """
         Args:
-            sorts: a dict map from variable(function) name to its sort
+            sorts: maps from variable name to variable
         """
         self.sorts = sorts
 
+        # names mapping a sequence number to a term
+        self.names = dict()
+
+        # clauses mapping a sequence number to a proof term
+        self.clauses = dict()
+
     def var_name(self, x):
-        return Var(x, self.sorts[a])
+        return self.sorts[x]
     
     def integer(self, num):
         return Int(num)
@@ -90,33 +154,116 @@ class TermTransformer(Transformer):
         return Real(num)
 
     def int_tm(self, var):
-        return Var(var, IntType)
+        return Var(var.value, IntType)
     
     def real_tm(self, var):
-        return Var(var, RealType)
+        return Var(var.value, RealType)
 
     def bool_tm(self, var):
-        return Var(var, BoolType)
+        return Var(var.value, BoolType)
 
     def comb_tm(self, *args):
-        pass
+        return args[0](*args[1:])
 
-@v_args(inline=True)
-class ProofTransformer(Transformer):
-    """Parse a proof step."""
-    def __init__(self):
-        pass
+    def uminus_tm(self, arg):
+        return Const("uminus", None)(arg)
+
+    def plus_tm(self, lhs, rhs):
+        return Const("plus", None)(lhs, rhs)
+
+    def minus_tm(self, lhs, rhs):
+        return Const("minus", None)(lhs, rhs)
+
+    def times_tm(self, lhs, rhs):
+        return Const("times", None)(lhs, rhs)
+
+    def greater_tm(self, lhs, rhs):
+        return Const("greater", None)(lhs, rhs)
+
+    def greater_eq_tm(self, lhs, rhs):
+        return Const("greater_eq", None)(lhs, rhs)
+
+    def less_tm(self, lhs, rhs):
+        return Const("less", None)(lhs, rhs)
+
+    def less_eq_tm(self, lhs, rhs):
+        return Const("less_eq", None)(lhs, rhs)
+
+    def neg_tm(self, tm):
+        return Not(tm)
+
+    def conj_tm(self, *tm):
+        return And(tm)
+
+    def disj_tm(self, *tm):
+        return Or(tm)
+
+    def implies_tm(self, s, t):
+        return Implies(s, t)
+
+    def names_tm(self, num, tm):
+        self.names[num] = tm
+        return tm
+
+    def repr_tm(self, num):
+        return self.names[num]
+
+    def exists_tm(self, v, t):
+        return Exists(v, t)
+
+    def forall_tm(self, v, t):
+        return Forall(v, t)
     
-    def var_name(self, a):
-        return Var(a)
+    def equals_tm(self, lhs, rhs):
+        return Const("equals", None)(lhs, rhs)
 
-term_parser = Lark(grammar, start="logical", parser="lalr", transformer=TermTransformer())
-proof_parser = Lark(grammar, start="proof", parser="lalr", transformer=ProofTransformer())
+    def pair_tm(self, s, t):
+        return (s, t)
+
+    def let_tm(self, *tms):
+        return [ProofTerm.assume(Eq(s, t)) for s, t in tms]
+
+    def name_let_tm(self, num, *tms):
+        return [ProofTerm.assume(Eq(s, t)) for s, t in tms]
+    
+    def concl_tm(self, *tms):
+        return Or(*tms)
+
+    def clause_name(self, cl):
+        return self.clauses[cl]
+
+    def clauses(self, *clauses):
+        return tuple(clauses)
+
+    def args(self, name):
+        """Used in forall_inst."""
+        return name
+
+    def step_proof(self, num, params, concl):
+        self.clauses[num] = concl
+        return Rule(num, params, concl)
+
+    def input_proof(self, num, concl):
+        self.clauses[num] = concl
+        return Input(num, concl)
+
+def term_parser(sorts):
+    return Lark(grammar, start="proof", parser="lalr", transformer=TermTransformer(sorts=sorts))
+
+type_parser = Lark(grammar, start="type", parser="lalr", transformer=TypeTransformer())
 
 def parse_term(s):
     """Parse a proof step."""
     try:
         return term_parser.parse(s)
+    except (exceptions.UnexpectedCharacters, exceptions.UnexpectedToken) as e:
+        print("When parsing:", s)
+        raise e
+
+def parse_type(s):
+    """Parse a proof step."""
+    try:
+        return type_parser.parse(s)
     except (exceptions.UnexpectedCharacters, exceptions.UnexpectedToken) as e:
         print("When parsing:", s)
         raise e
