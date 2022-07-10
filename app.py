6bcee10dfb3aebaa178fdc01500236745d60b4e0
Bohua Zhan <bzhan@ios.ac.cn>
2022-07-10 18:15:03 +0800

    Use nodemon for reloading.

--- app.py
+++ app.py
@@ -13,4 +13,4 @@ if __name__ == '__main__':
     app.jinja_env.variable_end_string = ']}'
 
 
-    app.run(host='127.0.0.1', port=port, debug=True)
+    app.run(host='127.0.0.1', port=port, debug=True, use_reloader=False)
