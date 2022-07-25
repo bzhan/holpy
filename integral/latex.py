4f42173554eb2ba3f869149c65e3603b2176088a
Bohua Zhan <bzhan@ios.ac.cn>
2022-07-25 14:42:10 +0800

    Improve latex.

--- integral/latex.py
+++ integral/latex.py
@@ -42,34 +42,32 @@ def convert_expr(e: expr.Expr, mode: str = "large") -> str:
             x, y = e.args
             sx = convert_expr(x, mode)
             sy = convert_expr(y, mode)
-            if e.op in ("+", "-", "^"):
-                if e.op == "^":
-                    # Still can improve 
-                    if y.ty == expr.CONST and isinstance(y.val, Fraction):
-                        if y.val.numerator == 1:
-                            if y.val.denominator == 2:
-                                return "\\sqrt{%s}" % sx
-                            else:
-                                return "\\sqrt[%s]{%s}" % (y.val.denominator, sx)
-                        if x.ty == OP:
-                            return "(%s) ^ {%s}" % (sx, sy)
-                        return "%s ^ {%s}" % (sx, sy)
-                    if y.ty == expr.CONST and y.val < 0:
-                        if y.val != -1:
-                            new_expr = expr.Op("/", expr.Const(1), expr.Op("^", x, expr.Const(-y.val)))
+            if e.op == '^':
+                if y.ty == expr.CONST and isinstance(y.val, Fraction):
+                    # When the exponent is a constant fraction, take care of root cases
+                    if y.val.numerator == 1:
+                        if y.val.denominator == 2:
+                            return "\\sqrt{%s}" % sx
                         else:
-                            new_expr = expr.Op("/", expr.Const(1), x)
-                        return convert_expr(new_expr)
-                    elif isinstance(x, expr.Fun) and len(x.args) > 0 and x.func_name != "abs":
-                        return "\%s^{%s}(%s)" % (x.func_name, sy, convert_expr(x.args[0]))                    
-                if x.priority() < expr.op_priority[e.op]:
-                    sx = "(%s)" % sx
-                if y.priority() < expr.op_priority[e.op]:
-                    sy = "{(%s)}" % sy
-                if e.op == "^" and len(sy) > 1:
+                            return "\\sqrt[%s]{%s}" % (y.val.denominator, sx)
+                    if x.ty == OP:
+                        return "(%s) ^ {%s}" % (sx, sy)
+                    else:
+                        return "%s ^ {%s}" % (sx, sy)
+                elif isinstance(x, expr.Fun) and len(x.args) > 0 and x.func_name != "abs":
+                    # If base is a trigonometric function, use special format
+                    return "\%s^{%s}(%s)" % (x.func_name, sy, convert_expr(x.args[0]))
+                else:
+                    # Ordinary cases
+                    if x.priority() <= e.priority():
+                        sx = "(%s)" % sx
                     sy = "{%s}" % sy
-                if y.ty == expr.OP and y.op == e.op and y.op in ("-", "/"):
-                    sy = "{(%s)}" % sy
+                    return "%s %s %s" % (sx, e.op, sy)
+            elif e.op == '+' or e.op == '-':
+                if x.priority() < e.priority():
+                    sx = "(%s)" % sx
+                if y.priority() <= e.priority():
+                    sy = "(%s)" % sy
                 return "%s %s %s" % (sx, e.op, sy)
             elif e.op == "*":
                 if not x.is_constant() and not y.is_constant() and not (y.ty == OP and y.op == "^" and y.args[1].ty == CONST and y.args[1].val < 0) or x == expr.Fun("pi") or y == expr.Fun("pi"):
@@ -88,14 +86,6 @@ def convert_expr(e: expr.Expr, mode: str = "large") -> str:
                     if x.ty == expr.CONST and isinstance(x.val, Fraction) and mode == "short":
                         sx = "(" + sx + ")"
                     return "%s %s" % (sx, sy)
-                elif x.is_constant() and y.ty == CONST and isinstance(y.val, Fraction) and y.val.numerator == 1 and y.val.denominator != 1:
-                    return "\\frac{%s}{%s}" % (sx, convert_expr(expr.Const(y.val.denominator)))
-                elif y.ty == OP and y.op == "^" and y.args[1].ty == CONST and y.args[1].val < 0:
-                    if y.args[1].val == -1:
-                        return "\\frac{%s}{%s}" % (sx, convert_expr(y.args[0]))
-                    else:
-                        new_denom = Op("^", y.args[0], Const(-y.args[1].val))
-                        return "\\frac{%s}{%s}" % (sx, convert_expr(new_denom, mode))
                 elif x.ty == expr.CONST:
                     if x.val == -1:
                         if y.ty == OP:
@@ -209,6 +199,6 @@ def convert_expr(e: expr.Expr, mode: str = "large") -> str:
         else:
             return "\\lim\\limits_{%s\\to %s} %s" % (e.var, lim, body)
     elif e.ty == expr.DERIV:
-        return "\\frac{d}{d%s} %s" % (e.var, e.body)
+        return "\\frac{d}{d%s} %s" % (e.var, convert_expr(e.body, mode))
     else:
         raise NotImplementedError
