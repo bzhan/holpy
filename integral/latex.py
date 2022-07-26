54c4cf25445be9e7118d25c13f9c3d2018e849af
Bohua Zhan <bzhan@ios.ac.cn>
2022-07-26 20:53:36 +0800

    Improvements to latex.

--- integral/latex.py
+++ integral/latex.py
@@ -43,105 +43,100 @@ def convert_expr(e: expr.Expr, mode: str = "large") -> str:
             sx = convert_expr(x, mode)
             sy = convert_expr(y, mode)
             if e.op == '^':
-                if y.ty == expr.CONST and isinstance(y.val, Fraction):
-                    # When the exponent is a constant fraction, take care of root cases
-                    if y.val.numerator == 1:
-                        if y.val.denominator == 2:
-                            return "\\sqrt{%s}" % sx
-                        else:
-                            return "\\sqrt[%s]{%s}" % (y.val.denominator, sx)
-                    if x.ty == OP:
-                        return "(%s) ^ {%s}" % (sx, sy)
+                if y.ty == expr.CONST and isinstance(y.val, Fraction) and y.val.numerator == 1:
+                    # Square root and other root cases
+                    if y.val.denominator == 2:
+                        return "\\sqrt{%s}" % sx
                     else:
-                        return "%s ^ {%s}" % (sx, sy)
+                        return "\\sqrt[%s]{%s}" % (y.val.denominator, sx)
                 elif isinstance(x, expr.Fun) and len(x.args) > 0 and x.func_name != "abs":
                     # If base is a trigonometric function, use special format
+                    sy = convert_expr(y, mode="short")
                     return "\%s^{%s}(%s)" % (x.func_name, sy, convert_expr(x.args[0]))
                 else:
                     # Ordinary cases
+                    sy = convert_expr(y, mode="short")
                     if x.priority() <= e.priority():
                         sx = "(%s)" % sx
                     sy = "{%s}" % sy
                     return "%s %s %s" % (sx, e.op, sy)
             elif e.op == '+' or e.op == '-':
+                # Ordinary cases
                 if x.priority() < e.priority():
                     sx = "(%s)" % sx
                 if y.priority() <= e.priority():
                     sy = "(%s)" % sy
                 return "%s %s %s" % (sx, e.op, sy)
             elif e.op == "*":
-                if not x.is_constant() and not y.is_constant() and not (y.ty == OP and y.op == "^" and y.args[1].ty == CONST and y.args[1].val < 0) or x == expr.Fun("pi") or y == expr.Fun("pi"):
-                    if x.ty == expr.OP and (x.op not in ("^", "*")) and not len(x.args) == 1:
-                        sx = "(" + sx + ")"
-                    if y.ty == expr.OP and y.op != "^":
-                        sy = "(" + sy + ")"
-                    return "%s %s" % (sx, sy)
-                elif x.is_constant() and y.is_constant() and y.ty != CONST and not (y.ty == OP and y.op in ("+", "-") or y.ty == CONST and isinstance(y.val, Fraction)):
-                    if x == Const(-1):
-                        return "-%s" % sy
-                    if x.ty == expr.OP and x.op != "^" and len(x.args) != 1:
-                        sx = "(" + sx + ")"
-                    if y.ty == expr.OP and not (len(y.args) == 2 and y.op == "^" or y.args[1].ty == CONST and y.args[1].val == Fraction(1/2)):
-                        sy = "(" + sy + ")"
-                    if x.ty == expr.CONST and isinstance(x.val, Fraction) and mode == "short":
-                        sx = "(" + sx + ")"
-                    return "%s %s" % (sx, sy)
-                elif x.ty == expr.CONST:
-                    if x.val == -1:
-                        if y.ty == OP:
-                            return "-(%s)" % sy
-                        else:
-                            return "-%s" % sy
-                    elif x.val == 1 and not y.ty == OP:
-                        if y.ty == OP:
-                            return "(%s)" % sy
-                        else:
-                            return "%s" % sy
-                    elif isinstance(x.val, Fraction) and x.val.numerator == 1 and y.ty not in (expr.INTEGRAL, expr.OP, expr.EVAL_AT):
-                        return "\\frac{%s}{%s}" % (sy, convert_expr(expr.Const(x.val.denominator)))
-                    elif y.ty in (expr.VAR, expr.FUN):
-                        if isinstance(x.val, Fraction):
-                            return "(%s) %s" % (sx, sy)
-                        else:
-                            return "%s %s" % (sx, sy)
-                    elif not y.is_constant():
-                        if y.ty == OP and y.op != '^':
-                            return "%s (%s)" % (sx, sy)
-                        elif y.ty == EVAL_AT:
-                            return "%s \\times (%s)" % (sx, sy)
-                        return "%s %s" % (sx, sy)
-                    elif y.ty != CONST and y.is_constant() and not (y.ty == OP and y.op in ('+', '-')):
-                        return "%s %s"%(sx, sy)
-                    elif y.ty == OP and y.op == "^" and not y.args[0].is_constant():
-                        return "%s %s" % (sx, sy)
-                    else:
-                        if x.priority() < expr.op_priority[e.op]:
-                            sx = "(%s)" % sx
-                        if y.priority() < expr.op_priority[e.op]:
-                            sy = "(%s)" % sy
-                        return "%s %s %s" % (sx, e.op, sy)
-                else:
-                    if x.priority() < expr.op_priority[e.op]:
-                        sx = "(%s)" % sx
-                    if y.priority() < expr.op_priority[e.op]:
-                        sy = "(%s)" % sy
-                    return "%s %s %s" % (sx, e.op, sy)
+                if x.priority() < e.priority():
+                    sx = "(%s)" % sx
+                if y.priority() <= e.priority():
+                    sy = "(%s)" % sy
+                return "%s %s" % (sx, sy)
+                # if not x.is_constant() and not y.is_constant() and not (y.ty == OP and y.op == "^" and y.args[1].ty == CONST and y.args[1].val < 0) or x == expr.Fun("pi") or y == expr.Fun("pi"):
+                #     if x.ty == expr.OP and (x.op not in ("^", "*")) and not len(x.args) == 1:
+                #         sx = "(" + sx + ")"
+                #     if y.ty == expr.OP and y.op != "^":
+                #         sy = "(" + sy + ")"
+                #     return "%s %s" % (sx, sy)
+                # elif x.is_constant() and y.is_constant() and y.ty != CONST and not (y.ty == OP and y.op in ("+", "-") or y.ty == CONST and isinstance(y.val, Fraction)):
+                #     if x == Const(-1):
+                #         return "-%s" % sy
+                #     if x.ty == expr.OP and x.op != "^" and len(x.args) != 1:
+                #         sx = "(" + sx + ")"
+                #     if y.ty == expr.OP and not (len(y.args) == 2 and y.op == "^" or y.args[1].ty == CONST and y.args[1].val == Fraction(1/2)):
+                #         sy = "(" + sy + ")"
+                #     if x.ty == expr.CONST and isinstance(x.val, Fraction) and mode == "short":
+                #         sx = "(" + sx + ")"
+                #     return "%s %s" % (sx, sy)
+                # elif x.ty == expr.CONST:
+                #     if x.val == -1:
+                #         if y.ty == OP:
+                #             return "-(%s)" % sy
+                #         else:
+                #             return "-%s" % sy
+                #     elif x.val == 1 and not y.ty == OP:
+                #         if y.ty == OP:
+                #             return "(%s)" % sy
+                #         else:
+                #             return "%s" % sy
+                #     elif isinstance(x.val, Fraction) and x.val.numerator == 1 and y.ty not in (expr.INTEGRAL, expr.OP, expr.EVAL_AT):
+                #         return "\\frac{%s}{%s}" % (sy, convert_expr(expr.Const(x.val.denominator)))
+                #     elif y.ty in (expr.VAR, expr.FUN):
+                #         if isinstance(x.val, Fraction):
+                #             return "(%s) %s" % (sx, sy)
+                #         else:
+                #             return "%s %s" % (sx, sy)
+                #     elif not y.is_constant():
+                #         if y.ty == OP and y.op != '^':
+                #             return "%s (%s)" % (sx, sy)
+                #         elif y.ty == EVAL_AT:
+                #             return "%s \\times (%s)" % (sx, sy)
+                #         return "%s %s" % (sx, sy)
+                #     elif y.ty != CONST and y.is_constant() and not (y.ty == OP and y.op in ('+', '-')):
+                #         return "%s %s"%(sx, sy)
+                #     elif y.ty == OP and y.op == "^" and not y.args[0].is_constant():
+                #         return "%s %s" % (sx, sy)
+                #     else:
+                #         if x.priority() < expr.op_priority[e.op]:
+                #             sx = "(%s)" % sx
+                #         if y.priority() < expr.op_priority[e.op]:
+                #             sy = "(%s)" % sy
+                #         return "%s %s %s" % (sx, e.op, sy)
+                # else:
+                #     if x.priority() < expr.op_priority[e.op]:
+                #         sx = "(%s)" % sx
+                #     if y.priority() < expr.op_priority[e.op]:
+                #         sy = "(%s)" % sy
+                #     return "%s %s %s" % (sx, e.op, sy)
             elif e.op == "/":
                 if mode == 'large':
-                    if sy == "1":
-                        return "%s" % sx
-                    elif x.ty == expr.OP:
-                        if x.op == "-" and len(x.args) == 1:
-                            # (-x) / y => - (x/y)
-                            sxx = convert_expr(x.args[0])
-                            return "-\\frac{%s}{%s}" % (sxx, sy)
-                        elif y.ty == expr.CONST:
-                            return "\\frac{1}{%s} \\times %s" % (sy, sx)
-                        else:
-                            return "\\frac{%s}{%s}" % (sx, sy)
-                    else:
-                        return "\\frac{%s}{%s}" % (sx, sy)
+                    return "\\frac{%s}{%s}" % (sx, sy)
                 else:
+                    if x.priority() < e.priority():
+                        sx = "(%s)" % sx
+                    if y.priority() <= e.priority():
+                        sy = "(%s)" % sy
                     return "%s/%s" % (sx, sy)
             elif e.op == "=":
                 return "%s = %s" % (sx, sy)
@@ -159,17 +154,18 @@ def convert_expr(e: expr.Expr, mode: str = "large") -> str:
                 if e.args[0] == expr.Const(1):
                     return "e"
                 else:
+                    sx = convert_expr(x, mode="short")
                     return "e^{%s}" % sx
             elif e.func_name == "sqrt":
                 return "\\sqrt{%s}" % sx
             elif e.func_name == "abs":
                 return "\\left| %s \\right|" % sx
             elif e.func_name == "atan":
-                return "\\arctan{%s}" % sx
+                return "\\arctan{(%s)}" % sx
             elif e.func_name == "asin":
-                return "\\arcsin{%s}" % sx
+                return "\\arcsin{(%s)}" % sx
             elif e.func_name == "acos":
-                return "\\arccos{%s}" % sx
+                return "\\arccos{(%s)}" % sx
             else:
                 return "\\%s{(%s)}" % (e.func_name, sx)
         elif len(e.args) == 2:
