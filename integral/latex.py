02114680c85ba6bf724aecb7187b6c676170c24e
fyh <1913068672@qq.com>
2022-08-28 02:47:31 +0800

    fix

--- integral/latex.py
+++ integral/latex.py
@@ -84,7 +84,7 @@ def convert_expr(e: expr.Expr, mode: str = "large") -> str:
                     # If left side is a fraction, add dot to reduce ambiguity
                     return "%s\\cdot %s" % (sx, sy)
                 elif x.is_constant() and y.is_constant():
-                    if y.is_fun() and y.func_name == 'sqrt' or y.is_power() and y.args[1].is_const() and y.args[1].val==(1/2):
+                    if not y.is_const():
                         return "%s %s"%(sx, sy)
                     # 2*1 = 2· 1
                     return "%s\\cdot %s" % (sx, sy)
