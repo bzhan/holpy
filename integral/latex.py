d1d71ab598e709c6acb7e9db45e18caf47e7c1e3
Bohua Zhan <bzhan@ios.ac.cn>
2023-01-25 12:41:20 +0800

    Fix display of log.

--- integral/latex.py
+++ integral/latex.py
@@ -53,11 +53,12 @@ def convert_expr(e: expr.Expr, mode: str = "large") -> str:
                     else:
                         return "\\sqrt[%s]{%s}" % (y.val.denominator, sx)
                 elif isinstance(x, expr.Fun) and len(x.args) > 0 and x.func_name != "abs" \
-                        and x.func_name in ('cos', 'sin', 'tan', 'sec', 'csc', 'cot'):
-                    # If base is a trigonometric function, use special format
+                        and x.func_name in ('log', 'cos', 'sin', 'tan', 'sec', 'csc', 'cot'):
+                    # If base is logarithmic or trigonometric function, use special format
                     sy = convert_expr(y, mode="short")
                     return "\%s^{%s}(%s)" % (x.func_name, sy, convert_expr(x.args[0]))
-                elif isinstance(x, expr.Fun) and len(x.args) > 0 and x.func_name not in("abs","factorial",'sqrt'):
+                elif isinstance(x, expr.Fun) and len(x.args) > 0 and \
+                        x.func_name not in ("abs", "factorial", "sqrt"):
                     sy = convert_expr(y, mode="short")
                     return "%s^{%s}(%s)" % (x.func_name, sy, convert_expr(x.args[0]))
                 elif isinstance(x, expr.Const) and x.val<0:
